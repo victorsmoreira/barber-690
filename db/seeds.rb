fcf5ac171435b4148784ba71d96885b9b2d84879
Victor Moreira <victorsmoreira@outlook.com>
2021-08-23 16:59:13 -0300

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: '123456',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.full_address
+  )
+end
+
+10.times do
+  Haircut.create(
+    user_id: rand(1..10),
+    style: Faker::Restaurant.name,
+    price: rand(10..50),
+    category: Faker::Restaurant.type
+  )
+end
