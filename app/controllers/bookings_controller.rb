6247be3cd422ab168eac5e84f305757913dd06af
eduardo <eduardo.brandiao.adm@gmail.com>
2021-08-23 18:08:16 -0300

    haircut-new-create

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,20 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to booking_path(@booking)
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:appointment, :rating, :price, :user_id, :haircut_id)
+  end
+end
