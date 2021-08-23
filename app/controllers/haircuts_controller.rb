a53d77fb4353959c22c468823e6a28c1b2ddd1d8
giovanni guadagnucci <giovanniguadag@gmail.com>
2021-08-23 18:38:21 -0300

    set haircut

--- app/controllers/haircuts_controller.rb
+++ app/controllers/haircuts_controller.rb
@@ -1,12 +1,18 @@
 class HaircutsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home index show]
-
+  before_action :set_haircut, only: %i[show]
 
   def index
     @haircuts = Haircut.all
   end
 
   def show
+    set_haircut
+  end
+
+  private
+
+  def set_haircut
     @haircut = Haircut.find(params[:id])
   end
 end
