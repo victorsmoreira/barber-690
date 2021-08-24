7cf1f62ebd869c73776b0c4640081eb6899111b7
Victor Moreira <victorsmoreira@outlook.com>
2021-08-24 10:51:34 -0300

    conflito corrigido no teste

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,14 +1,8 @@
 require "test_helper"
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
-<<<<<<< HEAD
-  # test "the truth" do
-  #   assert true
-  # end
-=======
   test "should get new" do
     get bookings_new_url
     assert_response :success
   end
->>>>>>> master
 end
