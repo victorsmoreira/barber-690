6637be9b5b5937b7f8cf604dbf5809c3498ab154
giovanni guadagnucci <giovanniguadag@gmail.com>
2021-08-24 15:10:47 -0300

    Controller e views do haircuts

--- app/controllers/haircuts_controller.rb
+++ app/controllers/haircuts_controller.rb
@@ -1,18 +1,45 @@
 class HaircutsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home index show]
-  before_action :set_haircut, only: %i[show]
+  before_action :set_haircut, only: %i[edit show update destroy]
+
+  def new
+    @haircut = Haircut.new
+  end
+
+  def create
+    @haircut = Haircut.new(haircut_params)
+    @haircut.save
+    redirect_to haircut_path(@haircut)
+  end
+
+  def edit; end
 
   def index
     @haircuts = Haircut.all
   end
 
-  def show
-    set_haircut
+  def show; end
+
+  def update
+    if @haircut.update(haircut_params)
+      redirect_to @haircut
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @haircut.destroy
+    redirect_to haircuts_path
   end
 
   private
 
   def set_haircut
     @haircut = Haircut.find(params[:id])
+  end
+
+  def haircut_params
+    params.require(:haircut).permit(:style, :category, :price, :user_id)
   end
 end
