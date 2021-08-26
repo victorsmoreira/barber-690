ab1e36c6107b7d04aa15f338dea7ab3e0d28420f
Victor Moreira <victorsmoreira@outlook.com>
2021-08-26 19:59:57 -0300

    marker. js navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,9 +25,11 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 import { initMapbox } from '../plugins/init_mapbox';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initUpdateNavbarOnScroll();
 });
