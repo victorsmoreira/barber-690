fb28885d0529902169fe579c278158f3b6762e67
giovanni guadagnucci <giovanniguadag@gmail.com>
2021-08-27 11:25:46 -0300

    Big logo

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,18 +1,21 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-home');
+  const navbar = document.querySelector(".navbar-home");
+  const logo = document.getElementById("logo");
   if (navbar) {
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-home-white');
-        navbar.classList.add('navbar-light');
-        navbar.classList.remove('navbar-dark');
+        navbar.classList.add("navbar-home-white");
+        navbar.classList.add("navbar-light");
+        logo.classList.add("small-logo");
+        navbar.classList.remove("navbar-dark");
       } else {
-        navbar.classList.remove('navbar-home-white');
-        navbar.classList.add('navbar-dark');
-        navbar.classList.remove('navbar-light');
+        navbar.classList.remove("navbar-home-white");
+        navbar.classList.add("navbar-dark");
+        navbar.classList.remove("navbar-light");
+        logo.classList.remove("small-logo");
       }
     });
   }
-}
+};
 
 export { initUpdateNavbarOnScroll };
